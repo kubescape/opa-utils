5174460ce21430dce48259deea7dc4e4faa7153a
Lior Alafi <lalafi@armosec.io>
2022-02-07 22:23:19 +0200

    added score =0 on empty framework

--- score/score_test.go
+++ score/score_test.go
@@ -4,6 +4,11 @@ import (
 	"testing"
 
 	"github.com/armosec/k8s-interface/workloadinterface"
+	"github.com/armosec/opa-utils/reporthandling"
+	"github.com/armosec/opa-utils/reporthandling/results/v1/reportsummary"
+	"github.com/armosec/opa-utils/reporthandling/results/v1/resourcesresults"
+	v2 "github.com/armosec/opa-utils/reporthandling/v2"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestReplicaScore(t *testing.T) {
@@ -40,5 +45,34 @@ func TestInactiveDaemonScore(t *testing.T) {
 	}
 }
 
-func TestFrameworkMock(t *testing.T) {
+func TestEmptyFrameworV2kMock(t *testing.T) {
+	s := NewScore(map[string]workloadinterface.IMetadata{})
+	report := &v2.PostureReport{
+		Attributes:           []reportsummary.PostureAttributes{},
+		CustomerGUID:         "",
+		ClusterName:          "",
+		ClusterCloudProvider: "",
+		ReportID:             "",
+		JobID:                "",
+		PaginationInfo:       v2.PaginationMarks{},
+		SummaryDetails:       reportsummary.SummaryDetails{Frameworks: []reportsummary.FrameworkSummary{{Name: "empty", Controls: reportsummary.ControlSummaries{}}}},
+		Results:              []resourcesresults.Result{},
+		Resources:            []reporthandling.Resource{},
+	}
+	err := s.CalculatePostureReportV2(report)
+
+	if err == nil || report.SummaryDetails.Frameworks[0].Score != 0.0 {
+		t.Errorf("empty framework should return an error and have score equals 0")
+	}
+}
+
+func TestEmptyFrameworV1kMock(t *testing.T) {
+	s := NewScore(map[string]workloadinterface.IMetadata{})
+	report := reporthandling.PostureReport{FrameworkReports: []reporthandling.FrameworkReport{{
+		Name:           "empty",
+		ControlReports: []reporthandling.ControlReport{},
+	}}}
+	s.Calculate(report.FrameworkReports)
+	assert.Equal(t, float32(0.0), report.FrameworkReports[0].Score, "empty framework should have score equals 0")
+
 }
