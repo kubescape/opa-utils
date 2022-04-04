3ec6c0179c51161ffc382166a6096f906c179f22
dwertent <dwertent@cyberarmor.io>
2022-04-04 15:19:51 +0300

    convert to int

--- reporthandling/apis/severity.go
+++ reporthandling/apis/severity.go
@@ -1,5 +1,13 @@
 package apis
 
+const (
+	SeverityUnknown  = iota
+	SeverityLow      = iota
+	SeverityMedium   = iota
+	SeverityHigh     = iota
+	SeverityCritical = iota
+)
+
 func ControlSeverityToString(baseScore float32) string {
 	/*
 		9+	Critical
@@ -8,17 +16,40 @@ func ControlSeverityToString(baseScore float32) string {
 		1-3	Low
 		0 Unknown
 	*/
-	if baseScore >= 9 {
+	switch ControlSeverityToInt(baseScore) {
+	case SeverityCritical:
 		return "Critical"
+	case SeverityHigh:
+		return "High"
+	case SeverityMedium:
+		return "Medium"
+	case SeverityLow:
+		return "Low"
+	default:
+		return "Unknown"
+	}
+
+}
+
+func ControlSeverityToInt(baseScore float32) int {
+	/*
+		0   Unknown
+		1	Low
+		2	Medium
+		3	High
+		4	Critical
+	*/
+	if baseScore >= 9 {
+		return SeverityCritical
 	}
 	if baseScore >= 7 {
-		return "High"
+		return SeverityHigh
 	}
 	if baseScore >= 4 {
-		return "Medium"
+		return SeverityMedium
 	}
 	if baseScore >= 1 {
-		return "Low"
+		return SeverityLow
 	}
-	return "Unknown"
+	return SeverityUnknown
 }
