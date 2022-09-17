b5a2c062ddc732b8e3c0356879734209d7697229
Vlad Klokun <vklokun@protonmail.ch>
2022-09-17 22:50:54 +0300

    feat: support severity counters in report summary
    Previously, we have added severity counters.
    This commit adds support for the severity counters to the report
    summary, namely:
    - Updating the counters whenever a new resources is added to the report.

--- reporthandling/results/v1/reportsummary/severitycounters.go
+++ reporthandling/results/v1/reportsummary/severitycounters.go
@@ -1,10 +1,13 @@
 package reportsummary
 
+import "github.com/kubescape/opa-utils/reporthandling/apis"
+
 type ISeverityCounters interface {
 	NumberOfResourcesWithCriticalSeverity() int
 	NumberOfResourcesWithHighSeverity() int
 	NumberOfResourcesWithMediumSeverity() int
 	NumberOfResourcesWithLowSeverity() int
+	Increase(severity string, amount int)
 }
 
 func (sc *SeverityCounters) NumberOfResourcesWithCriticalSeverity() int {
@@ -21,4 +24,25 @@ func (sc *SeverityCounters) NumberOfResourcesWithMediumSeverity() int {
 
 func (sc *SeverityCounters) NumberOfResourcesWithLowSeverity() int {
 	return sc.ResourcesWithLowSeverityCounter
+}
+
+// Increase increments the counter of a given severity by a given amount
+func (sc *SeverityCounters) Increase(severity string, amount int) {
+	var counterToIncrement *int
+
+	switch severity {
+	case apis.SeverityCriticalString:
+		counterToIncrement = &sc.ResourcesWithCriticalSeverityCounter
+	case apis.SeverityHighString:
+		counterToIncrement = &sc.ResourcesWithHighSeverityCounter
+	case apis.SeverityMediumString:
+		counterToIncrement = &sc.ResourcesWithMediumSeverityCounter
+	case apis.SeverityLowString:
+		counterToIncrement = &sc.ResourcesWithLowSeverityCounter
+	// Return without incrementing on unrecognized severities
+	default:
+		return
+	}
+
+	*counterToIncrement++
 }
