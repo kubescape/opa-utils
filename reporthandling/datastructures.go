4c5e7a6f74e1f8f9749a6c84c1eb03f2186533f5
rcohencyberarmor <rcohen@armosec.io>
2023-07-31 15:38:58 +0300

    add scanning scope to framework struct
    Signed-off-by: rcohencyberarmor <rcohen@armosec.io>

--- reporthandling/datastructures.go
+++ reporthandling/datastructures.go
@@ -72,15 +72,15 @@ type Control struct {
 	FixedInput            map[string][]string `json:"fixedInput,omitempty"`
 	RulesIDs              *[]string           `json:"rulesIDs,omitempty" bson:"rulesIDs,omitempty"`
 	armotypes.PortalBase  `json:",inline" bson:"inline"`
-	Control_ID            string        `json:"id,omitempty" bson:"id,omitempty"  `
-	ControlID             string        `json:"controlID" bson:"controlID"`
-	CreationTime          string        `json:"creationTime" bson:"creationTime"`
-	Description           string        `json:"description" bson:"description"`
-	Remediation           string        `json:"remediation" bson:"remediation"`
-	Rules                 []PolicyRule  `json:"rules" bson:"rules,omitempty"`
-	FrameworkNames        []string      `json:"frameworkNames,omitempty" bson:"frameworkNames,omitempty"`
-	BaseScore             float32       `json:"baseScore,omitempty" bson:"baseScore,omitempty"`
-	ARMOImprovementFactor float32       `json:"ARMOImprovementFactor,omitempty" bson:"ARMOImprovementFactor,omitempty"`
+	Control_ID            string         `json:"id,omitempty" bson:"id,omitempty"  `
+	ControlID             string         `json:"controlID" bson:"controlID"`
+	CreationTime          string         `json:"creationTime" bson:"creationTime"`
+	Description           string         `json:"description" bson:"description"`
+	Remediation           string         `json:"remediation" bson:"remediation"`
+	Rules                 []PolicyRule   `json:"rules" bson:"rules,omitempty"`
+	FrameworkNames        []string       `json:"frameworkNames,omitempty" bson:"frameworkNames,omitempty"`
+	BaseScore             float32        `json:"baseScore,omitempty" bson:"baseScore,omitempty"`
+	ARMOImprovementFactor float32        `json:"ARMOImprovementFactor,omitempty" bson:"ARMOImprovementFactor,omitempty"`
 	ScanningScope         *ScanningScope `json:"scanningScope,omitempty" bson:"scanningScope,omitempty"`
 }
 
@@ -97,8 +97,9 @@ type Framework struct {
 	TypeTags             []string  `json:"typeTags" bson:"typeTags"`
 	Controls             []Control `json:"controls" bson:"-"`
 	// for new list of  controls in POST/UPADTE requests
-	ControlsIDs *[]string                       `json:"controlsIDs,omitempty" bson:"controlsIDs,omitempty"`
-	SubSections map[string]*FrameworkSubSection `json:"subSections,omitempty" bson:"subSections,omitempty"`
+	ControlsIDs   *[]string                       `json:"controlsIDs,omitempty" bson:"controlsIDs,omitempty"`
+	SubSections   map[string]*FrameworkSubSection `json:"subSections,omitempty" bson:"subSections,omitempty"`
+	ScanningScope *ScanningScope                  `json:"scanningScope,omitempty" bson:"scanningScope,omitempty"`
 }
 
 type UpdatedFramework struct {
